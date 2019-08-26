eb574beb0012ac18da3ff69fd9b7d9938c6be862
Ana Maria Antoaneta Tiru <Ana.Tiru@insse.ro>
2019-08-26 15:38:29 +0300

    improve parallel

--- template.R
+++ template.R
@@ -57,19 +57,23 @@ scroll_inf <- function(drv){
                          browserName = "firefox", 
                          extraCapabilities = firefoxProxyProfile)  
 
-
   prod_links <- list()
   
   rmdSel$open()
   rmdSel$navigate(mag_list[[1]]$url_mag)
-  links_lvl1 <- read_html(rmdSel$getPageSource()[[1]]) %>% html_nodes(mag_list[[1]]$navigation$css_lvl1) %>% html_attr("href")
+  links_lvl1 <- read_html(rmdSel$getPageSource()[[1]]) %>%
+                html_nodes(mag_list[[1]]$navigation$css_lvl1) %>%
+                html_attr("href")
   for(j in links_lvl1){
     rmdSel$navigate(paste0(mag_list[[1]]$url_mag, j))
-    links_lvl2 <- read_html(rmdSel$getPageSource()[[1]]) %>% html_nodes(mag_list[[1]]$navigation$css_lvl2) %>% html_attr("href")
+    links_lvl2 <- read_html(rmdSel$getPageSource()[[1]]) %>%
+                  html_nodes(mag_list[[1]]$navigation$css_lvl2) %>%
+                  html_attr("href")
     for(k in links_lvl2){
       rmdSel$navigate(paste0(mag_list[[1]]$url_mag, k))
       scroll_inf(rmdSel) 
-      prod_links[[length(prod_links) + 1]] <- paste0(mag_list[[1]]$url_mag, k, read_html(rmdSel$getPageSource()[[1]]) %>%
+      prod_links[[length(prod_links) + 1]] <- paste0(mag_list[[1]]$url_mag, k,
+                                                     read_html(rmdSel$getPageSource()[[1]]) %>%
                                                        html_nodes(mag_list[[1]]$navigation$css_lvl3) %>%
                                                        html_attr("href"))
     }
@@ -77,7 +81,7 @@ scroll_inf <- function(drv){
   
   
   
-  cols <- unique(unlist(prod_links))    
+  links <- unique(unlist(prod_links))    
   ##############################################################################################################################
   ####################################################### Extraction ###########################################################
   
@@ -99,12 +103,12 @@ scroll_inf <- function(drv){
                            extraCapabilities = firefoxProxyProfile) 
   })
   
-  resp <- foreach(x=1:length(prod_links)) %dopar%{
+  resp <- foreach(x=1:4) %dopar%{
     rmdSel$open()
-    rmdSel$navigate(prod_links[x])
+    rmdSel$navigate(links[x])
     nume <- extractAttr(rmdSel$getPageSource()[[1]], mag_list[[1]]$extraction$nume, "href")
     pret <- extractAttr(rmdSel$getPageSource()[[1]], mag_list[[1]]$extraction$pret, "href")
-    #pret_cant <- extractAttr(rmdSel$getPageSource()[[1]], mag_list[[1]]$extraction$pret_cant)
+    pret_cant <- extractAttr(rmdSel$getPageSource()[[1]], mag_list[[1]]$extraction$pret_cant, "href")
     if(is.na(mag_list[[1]]$extraction$categorie)){
       #  categorie <-   
     } else{
@@ -115,12 +119,11 @@ scroll_inf <- function(drv){
     magazin <- mag_list[[1]]$nume
     date_extrase <- list(nume = nume, 
                          pret = pret, 
-                         #pret_cant = pret_cant, 
+                         pret_cant = pret_cant, 
                          categorie = categorie, 
                          descriere = descriere, 
                          data_extragere = data_extragere, 
                          magazin = magazin)
-    rmdSel$close()
   }
   
   clusterEvalQ(cl, {
@@ -132,76 +135,3 @@ scroll_inf <- function(drv){
   
   resp <- unlist(resp, recursive = FALSE)
   
-  
-  
-  
-  
-  
-  
-  
-    library(RSelenium)
-    library(rvest)
-    library(xml2)
-    rmdSel <- remoteDriver(remoteServerAddr = "127.0.0.1",
-                           port = 4444L,
-                           browserName = "firefox")
-    
-  nume <- c()
-  pret <- c()
-  categorie <- c()
-  descriere <- c()
-  data_extragere <- c()
-  magazin <- c()
-  for (i in 1:length(prod_links)) {
-    for (j in 1:length(prod_links[[i]])) {
-    rmdSel$open()
-    rmdSel$navigate(prod_links[[i]][j])
-    num <- extractAttr(prod_links[[i]][j], mag_list[[1]]$extraction$nume, "href")
-    pre <- extractAttr(prod_links[[i]][j], mag_list[[1]]$extraction$pret, "href")
-    #pret_cant <- extractAttr(prod_links[[i]][1], mag_list[[1]]$extraction$pret_cant, "href")
-    if(is.na(mag_list[[1]]$extraction$categorie)){
-      #  categorie <-   
-    } else{
-      categ <- extractAttr(prod_links[[i]][j], mag_list[[1]]$extraction$categorie, "href")
-    }
-    desc <- extractAttr(prod_links[[i]][j], mag_list[[1]]$extraction$descriere, "href")
-    data_extr <- Sys.Date()
-    mag <- mag_list[[1]]$nume
-    nume <- c(nume,num)
-    pret <- c(pret,pre)
-    categorie <- c(categorie,categ)
-    descriere <- c(descriere,desc)
-    data_extragere <- c(data_extragere,data_extr)
-    magazin <- c(magazin, mag)
-    rmdSel$close()
-  }
-}
-  df <- data.frame(nume,pret,categorie, descriere, data_extragere, magazin)
-    
- 
-
-
-write.csv(resp, paste0(mag_list$nume, ".csv"), row.names = FALSE)
-
-set_config(use_proxy("http://proxy.insro.local", 8080))
-
-rmdSel <- remoteDriver(remoteServerAddr = "127.0.0.1",
-                       port = 4444L,
-                       browserName = "firefox")
-
-rmdSel$open()
-rmdSel$navigate("https://www.cora.ro")
-
-driver <- rsDriver(browser=c("firefox"))
-driver$client$navigate("https://www.cora.ro")
-
-
-
-links_lvl1 <- read_html(rmdSel$getPageSource()[[1]]) %>% html_nodes(mag_list[[1]]$navigation$css_lvl1) %>% html_attr("href")
-library(dplyr)
-
-
-driver<- rsDriver(browser=c("firefox"))
-rmdSel <- driver[["client"]]
-rmdSel$open()
-rmdSel$navigate("https://www.cora.ro")
